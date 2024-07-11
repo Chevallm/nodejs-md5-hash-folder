7736db72c51d471ecf1e1c12350acc139dd3efdf
Chevallm <maxime.chevallier_pichon@yahoo.fr>
2024-07-11 15:06:07 +0200

    remove duplicated files

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 const crypto = require('crypto');
 const fs = require('fs').promises;
-var path = require('path');
+const path = require('path');
 
 function hashMd5(arg) {
-  try {
-    return crypto.createHash('md5').update(arg).digest('hex');
-  } catch (e) {
-    throw new Error(e);
-  }
+  return crypto.createHash('md5').update(arg).digest('hex');
 }
 
-function hashFolder(folder, tree = new Map(), root = folder) {
-  return new Promise(async (resolve, reject) => {
+async function hashFolder(folder, tree = new Map(), root = folder) {
+  try {
     const files = await fs.readdir(folder);
-    for (let file of files) {
+    for (const file of files) {
       const filepath = path.join(root, file);
       const stats = await fs.stat(filepath);
       if (stats.isFile()) {
         const content = await fs.readFile(filepath);
         tree.set(filepath, hashMd5(content));
       } else {
-        hashFolder(filepath, tree, `${root}/${file}`);
+        await hashFolder(filepath, tree, path.join(root, file));
       }
     }
-
-    console.log(tree);
-    resolve(tree);
-  });
+    return tree;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
-hashFolder('test');
+hashFolder('test')
+  .then((tree) => console.log(tree))
+  .catch((error) => console.error('Error:', error));
